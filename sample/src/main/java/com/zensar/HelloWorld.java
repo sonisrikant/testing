925c8495e1c5e48797f1c611192433f75f78b4a5
sonisrikant <srikantsoni4692@gmail.com>
2020-10-22 14:59:43 +0530

    first commit

---
+++ sample/src/main/java/com/zensar/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.zensar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<html><body>");
+		out.println("Hello Testing!");
+		out.println("</body></html>");
+		out.close();
+	}
+}
